e51f4c6dc42ec1e9d5dd1a6af304f0c149352923
NewUser <newuser@MacBook-Pro-3.local>
2024-09-27 13:27:49 -0400

    update state menagement to reducer

--- client/src/contexts/PostContext.js
+++ client/src/contexts/PostContext.js
@@ -1,14 +1,53 @@
-import { createContext, useContext, useMemo, useState, useEffect } from 'react'
+import {
+    createContext,
+    useContext,
+    useMemo,
+    useReducer,
+    useEffect,
+} from 'react'
 import { useParams } from 'react-router-dom'
 import { useAsync } from '../hooks/useAsync'
 import { getPost } from '../services/posts'
+
+const commentsReducer = (state, action) => {
+    switch (action.type) {
+        case 'SET_COMMENTS':
+            return action.payload
+        case 'CREATE_COMMENT':
+            return [action.payload, ...state]
+        case 'UPDATE_COMMENT':
+            return state.map((comment) =>
+                comment.id === action.payload.id
+                    ? { ...comment, message: action.payload.message }
+                    : comment
+            )
+        case 'DELETE_COMMENT':
+            return state.filter((comment) => comment.id !== action.payload)
+        case 'TOGGLE_LIKE':
+            return state.map((comment) => {
+                if (comment.id === action.payload.id) {
+                    const updatedLikeCount = action.payload.addLike
+                        ? comment.likeCount + 1
+                        : comment.likeCount - 1
+                    return {
+                        ...comment,
+                        likeCount: updatedLikeCount,
+                        likedByMe: action.payload.addLike,
+                    }
+                }
+                return comment
+            })
+        default:
+            return state
+    }
+}
 
 const Context = createContext()
 
 function PostProvider({ children }) {
     const { id } = useParams()
     const { loading, error, value: post } = useAsync(() => getPost(id), [id])
-    const [comments, setComments] = useState([])
+    const [comments, dispatch] = useReducer(commentsReducer, [])
 
     const commentsByParentId = useMemo(() => {
         const group = {}
@@ -19,58 +58,28 @@ function PostProvider({ children }) {
         return group
     }, [comments])
 
-    function createLocalComment(comment) {
-        setComments((prevComments) => {
-            return [comment, ...prevComments]
-        })
-    }
-    function updateLocalComment(id, message) {
-        setComments((prevComments) => {
-            return prevComments.map((comment) => {
-                if (comment.id === id) {
-                    return { ...comment, message }
-                } else {
-                    return comment
-                }
-            })
-        })
-    }
-    function deleteLocalComment(id) {
-        setComments((prev) => {
-            return prev.filter((comment) => comment.id !== id)
-        })
-    }
-
-    function toggleLocalCommentLike(id, addLike) {
-        setComments((prevComments) => {
-            return prevComments.map((comment) => {
-                if (id === comment.id) {
-                    if (addLike) {
-                        return {
-                            ...comment,
-                            likeCount: comment.likeCount + 1,
-                            likedByMe: true,
-                        }
-                    } else {
-                        return {
-                            ...comment,
-                            likeCount: comment.likeCount - 1,
-                            likedByMe: false,
-                        }
-                    }
-                } else {
-                    return comment
-                }
-            })
-        })
-    }
-
     useEffect(() => {
         if (post?.comments == null) return
-        setComments(post.comments)
-    }, [post?.comments])
+        dispatch({ type: 'SET_COMMENTS', payload: post.comments })
+    }, [post])
 
-    function getReplies(parentId) {
+    const createLocalComment = (comment) => {
+        dispatch({ type: 'CREATE_COMMENT', payload: comment })
+    }
+
+    const updateLocalComment = (id, message) => {
+        dispatch({ type: 'UPDATE_COMMENT', payload: { id, message } })
+    }
+
+    const deleteLocalComment = (id) => {
+        dispatch({ type: 'DELETE_COMMENT', payload: id })
+    }
+
+    const toggleLocalCommentLike = (id, addLike) => {
+        dispatch({ type: 'TOGGLE_LIKE', payload: { id, addLike } })
+    }
+
+    const getReplies = (parentId) => {
         return commentsByParentId[parentId]
     }
 
@@ -96,6 +105,8 @@ function PostProvider({ children }) {
         </Context.Provider>
     )
 }
+
+export default PostProvider
 
 function usePost() {
     const context = useContext(Context)
